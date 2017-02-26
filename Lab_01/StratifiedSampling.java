b89e9c2dd8e3d4f5f967ee061e45ccd69b85aca5
Mary Danielle C. Amora <mcamora@up.edu.ph>
2017-02-26 17:11:57 +0800

    Update StratifiedSampling.java

--- Lab_01/StratifiedSampling.java
+++ Lab_01/StratifiedSampling.java
@@ -1,110 +1,83 @@
-package Lab01;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.lang.*;
 import java.util.Scanner;
 
-import static Lab01.SeratoAndAmora.gather;
-
 /**
- * Created by Serato & Amora on February 19, 2017
+ * Created by Mary Danielle C. Amora on 2/26/2017.
  */
-public class StratifiedSampling extends Sampling implements DoesntNeedSampleSize{
+public class StratifiedSampling extends Sampling{
+
+    private ArrayList<String> sample = new ArrayList<>();
+    private ArrayList<String> indices = new ArrayList<>();
+    private ArrayList<String> unique = new ArrayList<>();
+
     StratifiedSampling(ArrayList<String> sampleFrame) {
-        super(sampleFrame, "STRATIFIED SAMPLING");
+        super(sampleFrame, "STRATIFIED SAMPLE");
     }
+
 
     @Override
     public ArrayList<String> getSample(int sampleSize) {
-        ArrayList<String>[] strata = new ArrayList[50];
-        float percentage;
-        ArrayList<String> heads = new ArrayList<>();
-
-        ArrayList<String> sample = new ArrayList<>();
-
-        for (String data: sampleFrame){
-            if (!heads.contains(data)){
-                heads.add(data);
+        double percentage;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Percentage of elements you want from each stratum: ");
+        percentage = scan.nextDouble();
+        for (String s: sampleFrame){
+            if (!unique.contains(s)){
+                unique.add(s);
             }
         }
+        //unique elements
+       /*
+        for (String s: unique){
+            System.out.print(s+">> ");
+        }
 
-        Scanner forPercentage = new Scanner(System.in);
-
-        System.out.println("Please specify a percentage for each stratum: ");
-        percentage = forPercentage.nextFloat();
-        int i = 0;
-        for (String head: heads){
-            ArrayList<String> temp = new ArrayList<>();
-            for (String data: sampleFrame){
-                if (head.equals(data)){
-                    temp.add(data);
+        System.out.println(); */
+        for (String s: unique){
+            int i = 0;
+            for (String str: sampleFrame){
+                if (s.equals(str)){
+                    indices.add(i+"");
                 }
+                i++;
             }
-            strata[i] = temp;
+            //printing
+            /*
+            System.out.print(s+" >> ");
+            for (String m: indices){
+                System.out.print(m+", ");
+            }
+            */
+
+            SimpleRandomSampling sampleThis = new SimpleRandomSampling(indices);
+            sample.addAll(sampleThis.getSample((int)(Math.ceil(indices.size()*(percentage/100)))));
+           /*
+            System.out.println("SAMPLE: ");
+            for (String a: sample){
+                System.out.print(a+ ", ");
+            }
+            System.out.println("--end of sample--"); */
+            indices.clear();
+        }
+
+        indices.addAll(sample);
+        sample.clear();
+
+        //printing the sample
+
+        int i = 0;
+        System.out.println("Sample: ");
+        for (String st: indices){
+            int index = Integer.parseInt(st);
+            System.out.print("Index "+ (index + 1));
+            sample.add(sampleFrame.get(index));
+            System.out.print(" = "+ sample.get(i)+"; ");
             i++;
-            temp.clear();
         }
+        System.out.println();
 
-        //generating randomly
-
-        for (i = 0; strata[i] != null; i++){
-            SimpleRandomSampling sampleThis = new SimpleRandomSampling(strata[0]);
-            sample.addAll(sampleThis.getSample());
-        }
 
         return sample;
     }
-
-
-    /*
-    @Override
-    public ArrayList<String> getSample(int sampleSize) {
-        ArrayList<String> sample;
-        if (numeric) { // stratify to 10 strata
-            // finding the maximum
-            int max = 0;
-            for (String num : sampleFrame) {
-                if (max < Integer.parseInt(num)) {
-                    max = Integer.parseInt(num);
-                }
-            }
-            int toDivide = (max / 10) + 1;
-            int lowerLimit = 0, upperLimit = toDivide;
-            for(int i = 0; i < 10; i++, lowerLimit = upperLimit + 1, upperLimit = toDivide * (i + 1)) {
-                sample = new ArrayList<>();
-                for (String item : sampleFrame) {
-                    int val = Integer.parseInt(item);
-                    if (lowerLimit <= val && val <= upperLimit) {
-                        sample.add(item);
-                    }
-                }
-                gather.printFrame(sample, "STRATA " + (i + 1) + " - " + lowerLimit + " - " + upperLimit);
-                if (sample.size() == 0) {
-                    System.out.println("<EMPTY>");
-                }
-            }
-        } else {
-            // sort alphabetically
-            int i = 0;
-            for (char c = 'A', d = (char) (c + 4); d <= 'Z';  c += 5, d = (char) (c + 4)) {
-                if ('Z' - c <= 5) {
-                    d = 'Z';
-                }
-                sample = new ArrayList<>();
-                for (String item : sampleFrame) {
-                    if ((item.charAt(0) >= c && item.charAt(0) <= d)|| (item.charAt(0) >= c + 32 && item.charAt(0) <= d + 32)) {
-                        sample.add(item);
-                    }
-                }
-                gather.printFrame(sample, "STRATA " + ++i + " - " + c + " - " + d);
-                if (sample.size() == 0) {
-                    System.out.println("<EMPTY>");
-                }
-            }
-        }
-        return  null;
-    }
-    */
 }
