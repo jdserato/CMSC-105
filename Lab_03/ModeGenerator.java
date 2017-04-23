6ac80e06ff36b1f2261beb94f400334fba404e4c
Mary Danielle C. Amora <mcamora@up.edu.ph>
2017-04-23 21:29:48 +0800

    Update ModeGenerator.java

--- Lab_03/ModeGenerator.java
+++ Lab_03/ModeGenerator.java
@@ -2,21 +2,60 @@ package Lab03;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Created by localuser on 4/21/17.
  */
 public class ModeGenerator {
 
+    private LinkedList<Data> dataCount = new LinkedList<>();
+
     public ModeGenerator() {
         //constructor
     }
 
-    public ArrayList<Double> getMode(ArrayList<Double> data){
-
+    private ArrayList<Data> countMode(){
+        int maximum = 0;
+        ArrayList<Data> finalMode = new ArrayList<>();
+        for (Data findMax: dataCount){
+            if (findMax.getCount() > maximum){
+                maximum = findMax.getCount();
+            }
+        }
+        for (Data addThis: dataCount){
+            if (addThis.getCount() == maximum){
+                finalMode.add(addThis);
+            }
+        }
+        return finalMode;
     }
 
+    public ArrayList<Data> getMode(ArrayList<Double> data){
+        boolean checking = false;
+        for (Double d: data){
+            for (Data check: dataCount){
+                if (check.getName().equals(d+"")){
+                    check.addCount();
+                    checking = true;
+                    break;
+                }
+            }
+            if (checking){
+                //do nothing
+            }else{
+                Data newData = new Data(d+"",1);
+                dataCount.add(newData);
+            }
+            checking = false;
+        }
+        //countMode
+        return countMode();
+    }
+
+    /*
     public Double getModalClass(){
 
     }
+    */
 }
